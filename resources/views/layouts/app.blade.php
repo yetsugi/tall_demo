b6d3cac1ba97916b0777731be79d5669cce3d437
Yetsugi <yetsugi@gmail.com>
2022-03-24 10:28:53 +0100

    Tailwind, Livewire, Debugbar

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>Laravel</title>
+
+        <livewire:styles />
+        
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+        
+        <script src="{{ mix('js/app.js') }}" defer></script>
+    </head>
+    <body>
+        {{ $slot }}
+
+        <livewire:scripts />
+    </body>
+</html>
---
+++ resources/views/livewire/users/index.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Users Index</h1>
+</div>
